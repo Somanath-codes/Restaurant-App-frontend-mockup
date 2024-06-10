b659e27a06f0ca38fdfc9dcf3e14665c73d1be05
Somanath Nayak <138901696+Somanath-codes@users.noreply.github.com>
2024-06-10 15:02:33 +0530

    Update OTP_latest.py

--- OTP_latest.py
+++ OTP_latest.py
@@ -5,8 +5,8 @@ def send_email(email_variable):
     x=random.randint(100000,999999)
     server=smtplib.SMTP('smtp.gmail.com',587)
     server.starttls()
-    server.login('kmitgotnochill@gmail.com','zgprrtpfydxretwh')
-    server.sendmail('kmitgotnochill@gmail.com',email_variable,' Thank You for Choosing Mitahara, Your OTP is %d , Please do not share it with anyone. '%(x))
+    server.login('some@email.com','api_key')
+    server.sendmail('some@email.com',email_variable,' Thank You for Choosing Mitahara, Your OTP is %d , Please do not share it with anyone. '%(x))
     print('Mail Sent')
     return x
 
